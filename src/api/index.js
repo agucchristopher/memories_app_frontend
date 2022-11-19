3ab5c334e3c34efdf64e3e8fb2a0be494db7c538
Agu C Christopher <aguchris740@gmail.com>
2022-11-19 12:57:16 -0800

    Fourth Commit

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 const url = 'https://chris-memories-backend.herokuapp.com/';
 
 export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
+export const createPost = (newPost) => {
+    axios.post(url, newPost)
+    console.log(newPost)
+};
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
