c686e7c4870e70625098a94d2b45b77377b530c4
Agu C Christopher <aguchris740@gmail.com>
2022-11-19 11:17:14 -0800

    Third Commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://mynodejs-backend-api.herokuapp.com/';
+const url = 'https://chris-memories-backend.herokuapp.com/';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
