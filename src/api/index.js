967525d67d4f7bd4bfb98cff56f3dbebd3961dbe
Agu C. Christopher <aguchris740@gmail.com>
2023-03-29 01:34:24 +0100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,8 @@ const url = 'https://memories-app-backend.vercel.app/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => {
-    axios.post(url, newPost)
-    console.log(newPost)
+    axios.post(url, newPost);
+   // = console.log(newPost)
 };
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
